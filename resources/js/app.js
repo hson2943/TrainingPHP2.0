33743902805fa6659aa34bf2413c3aa776b6ee7a
hson2943 <balaba294313@gmail.com>
2023-06-25 18:42:51 +0700

    connect between vue and laravel

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
 import './bootstrap';
+import { createApp } from 'vue';
+import App from './app.vue'
+
+
+createApp(App).mount("#app")
---
+++ resources/js/app.vue
@@ -0,0 +1,5 @@
+<template>
+<router-link>
+    123
+</router-link>
+</template>
